db6061b45bf9759d1a009231f2f97ea5c84777e5
thundermol <UUrb73b234>
2019-09-18 11:51:31 +0200

    Test

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,7 +1,7 @@
 <template>
-    <section>
+    <main>
         <router-view/>
-    </section>
+    </main>
 </template>
 
 <script>
--- src/components/pages/Contact.vue
+++ src/components/pages/Contact.vue
@@ -1,100 +1,25 @@
 <template>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6">
-        <div class="form-group">
-          <label for="form_name">Firstname *</label>
-          <input
-            id="form_name"
-            type="text"
-            name="name"
-            class="form-control"
-            placeholder="Please enter your firstname *"
-            required="required"
-            data-error="Firstname is required.">
-          <div class="help-block with-errors"/>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="form-group">
-          <label for="form_lastname">Lastname *</label>
-          <input
-            id="form_lastname"
-            type="text"
-            name="surname"
-            class="form-control"
-            placeholder="Please enter your lastname *"
-            required="required"
-            data-error="Lastname is required.">
-          <div class="help-block with-errors"/>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-6">
-        <div class="form-group">
-          <label for="form_email">Email *</label>
-          <input
-            id="form_email"
-            type="email"
-            name="email"
-            class="form-control"
-            placeholder="Please enter your email *"
-            required="required"
-            data-error="Valid email is required.">
-          <div class="help-block with-errors"/>
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="form-group">
-          <label for="form_phone">Phone</label>
-          <input
-            id="form_phone"
-            type="tel"
-            name="phone"
-            class="form-control"
-            placeholder="Please enter your phone">
-          <div class="help-block with-errors"/>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12">
-        <div class="form-group">
-          <label for="form_message">Message *</label>
-          <textarea
-            id="form_message"
-            name="message"
-            class="form-control"
-            placeholder="Message for me *"
-            rows="4"
-            required="required"
-            data-error="Please,leave us a message."/>
-          <div class="help-block with-errors"/>
-        </div>
-      </div>
-      <div class="col-md-12">
-        <input
-          type="submit"
-          class="btn btn-success btn-send"
-          value="Send message">
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12">
-        <p class="text-muted"><strong>*</strong> These fields are required.</p>
-      </div>
-  </div>  </div>
+    <b-container>
+        <b-row>
+            <b-col>
+               {{ text }}
+              <br/>
+                <b-link target="_blank" href="https://www.linkedin.com/in/robvanseventer/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BuVOUONRNRoOUEXsSC1Q%2FeA%3D%3D">
+                    My LinkedIn profile
+                </b-link>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
-const text = 'Here, you can contact me if you want.'
-export default {
-  name: 'Contact',
-  data () {
-    return {
-      text: text
+    const text = 'Here you can contact me if you want.'
+    export default {
+        name: 'Contact',
+        data() {
+            return {
+                text: text
+            }
+        }
     }
-  }
-}
 </script>
